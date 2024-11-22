ab2ee206915ab8b4f30c53aee4cac892c81169bd
VensCuyy <vinss306@gmail.com>
2024-11-22 14:40:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/*
+[+] =============================================== [+]
+[+]                                                            [+]
+[+] - INSTALLER PTERODACTYL                              [+]
+[+]                                                            [+]
+[+]  TQTO :                                                   [+]
+[+] • WannOffc ( MySelf)                                     [+]
+[+] • Creator Bot WhatsApp & Telegram                     [+]
+[+] • Para Pengguna Bot Tele & Wa                         [+]
+[+]                                                            [+]
+[+] © CreateByWannFyy                                      [+]
+[+] =============================================== [+]
+*/
+
+const config = {
+  token: '8104727507:AAEV1TeRfkARyxfmiPPF0uEe73Ac6tMqKxE',
+  adminId: '6766816191', 
+};
+
+module.exports = config;
